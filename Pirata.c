e20a9db69b4ef581c869dc0819b4a136b9e1ad3c
NahuelLV <130933110+NahuelLV@users.noreply.github.com>
2024-05-14 15:46:02 -0300

    Update Pirata.c

--- Pirata.c
+++ Pirata.c
@@ -8,8 +8,16 @@ void inicializarTablero(int arr[][100], int n) {
         arr[n - 1][i] = -1;     // Fila inferior
         arr[i][0] = -1;         // Columna izquierda
         arr[i][n - 1] = -1;     // Columna derecha
+        arr[n-1][0] = 3;        // Esquina inferior izquierda
+        arr[n-n][n-1] = 3;      // Esquina superior derecha
     }
+    int pirataX = rand() % n;
+    int pirataY = rand() % n; 
+    arr[pirataX][pirataY] = 2;
+        
 }
+
+
 
 void dibujarTablero(int arr[][100], int n) {
     for (int i = 0; i < n; i++) {
@@ -20,10 +28,16 @@ void dibujarTablero(int arr[][100], int n) {
     }
 }
 
+
+
 int main() {
-    srand(time(NULL));  
     int n;
-    printf("Ingrese el tamaño del tablero: ");
+    char opcion;
+    srand(time(NULL));  
+    printf("Hola, Bienvenido al juego del pirata!,\n Tu objetivo es encontrar el tesoro, quiere empezar?, y/n:");
+    scanf("%s", &opcion);
+    if(opcion == 'Y' || opcion == 'y'){
+        printf("Ingrese el tamaño del tablero, este va a determinar tamaño del mapa de juego(Max = 100): ");
     scanf("%d", &n);
 
     int arr[100][100]; 
@@ -31,6 +45,10 @@ int main() {
     // Inicializar y dibujar el tablero
     inicializarTablero(arr, n);
     dibujarTablero(arr, n);
-
+    }
+   else if(opcion == 'N' || opcion == 'n'){
+    printf("Bueno, andate a cagar");
+    return 0;
+   }
     return 0;
 }
